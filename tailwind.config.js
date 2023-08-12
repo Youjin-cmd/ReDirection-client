297b19d3ecb27a4df3d8be7ede893f70e6e2af48
Youjin-cmd <y.kimiau@gmail.com>
2023-08-13 01:35:46 +0900

    Fix. tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      red: "C32F2F",
-      hoverRed: "D94646",
-      pureRed: "FF0000",
-      lightRed: "F4ECEC",
-      black: "000000",
+      red: "#C32F2F",
+      hoverRed: "#D94646",
+      pureRed: "#FF0000",
+      lightRed: "#F4ECEC",
+      black: "#000000",
     },
   },
   plugins: [],
