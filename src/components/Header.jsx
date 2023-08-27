8ca9705454589a68bdac8e7a13802bcd38d548c6
Youjin-cmd <y.kimiau@gmail.com>
2023-08-27 21:46:12 +0900

    Feat. put navigate to main fn to Header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../shared/Button";
+
 function Header() {
+  const navigate = useNavigate();
+
+  function handleClickHome() {
+    navigate("/");
+  }
+
   return (
     <div className="flex items-end w-full h-min-[100px] p-5 pt-12">
-      <img
-        src="/assets/logo_rectangle.svg"
-        alt="decoration for logo"
-        className="absolute w-8 top-[40px] left-[28px]"
-      />
-      <img src="/assets/logo.svg" alt="logo" className="z-0 mr-10" />
+      <Button className="hover:cursor-pointer" onClick={handleClickHome}>
+        <img src="/assets/logo.svg" alt="logo" className="mr-10" />
+        <img
+          className="absolute w-8 top-[40px] left-[28px] -z-10"
+          src="/assets/logo_rectangle.svg"
+          alt="decoration for logo"
+        />
+      </Button>
       <div>
         <span className="truncate h-10">
           Convert your horizontal video to vertical video with motion analysis,
