18261968a57f3c7402d97aa1f7fb0bd2965ab990
Youjin-cmd <y.kimiau@gmail.com>
2023-08-29 19:34:51 +0900

    Setting. disable eslint regarding empty dependency

--- src/components/DropInput.jsx
+++ src/components/DropInput.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useNavigate } from "react-router-dom";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../shared/Button";
