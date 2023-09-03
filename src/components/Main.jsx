10b87181a777b36b54f948b0649ee7b883c328a8
Youjin-cmd <y.kimiau@gmail.com>
2023-09-03 17:04:11 +0900

    Feat. randomly show images on the main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,17 @@
 import DropInput from "./DropInput";
 
 function Main() {
+  const randomIndex = Math.floor(Math.random() * 2);
+
+  const imageSrc =
+    randomIndex === 0
+      ? "/assets/main_image_1_dog.png"
+      : "/assets/main_image_2_skate.png";
+
   return (
     <div className="flex items-center h-full">
       <div className="relative flex-none w-[1000px]">
-        <img
-          className="w-full"
-          src="/assets/main_image_1_dog.png"
-          alt="dog image"
-        />
+        <img className="w-full" src={imageSrc} alt="dog image" />
         <img
           className="absolute top-[-25px] left-[530px] w-[300px] animate-pulse"
           src="/assets/image_rectangle.svg"
