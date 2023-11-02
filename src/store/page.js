ae00bac6593d3482cfeb15ec5fc65dce9ccd2b44
Youjin-cmd <y.kimiau@gmail.com>
2023-11-02 21:59:05 +0900

    Fix. remove phrase from the main page

--- src/store/page.js
+++ src/store/page.js
@@ -1,8 +1,7 @@
 import { create } from "zustand";
 
 const usePageStore = create(set => ({
-  currentPage:
-    "Convert your horizontal video to vertical video with motion analysis, for free!",
+  currentPage: "",
   setCurrentPage: newPage => set({ currentPage: newPage }),
 }));
 
