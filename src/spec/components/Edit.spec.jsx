95c3e5d57a0d34488101d0caeac83543c2050635
Youjin-cmd <y.kimiau@gmail.com>
2023-11-13 22:31:12 +0900

    Test. update spec for Edit

--- src/spec/components/Edit.spec.jsx
+++ src/spec/components/Edit.spec.jsx
@@ -4,7 +4,12 @@ import Edit from "../../components/Edit";
 const mocks = {
   navigate: vi.fn(),
   location: { state: "something" },
+  postEditReuqest: vi.fn(),
 };
+
+vi.mock("../../apis/usePostEditRequest", () => ({
+  default: () => mocks.postEditReuqest,
+}));
 
 vi.mock("react-router-dom", async () => {
   const actual = await vi.importActual("react-router-dom");
@@ -17,17 +22,17 @@ vi.mock("react-router-dom", async () => {
 });
 
 describe("Edit component", () => {
-  it("renders button well", () => {
+  beforeEach(() => {
     render(<Edit />);
+  });
 
+  it("renders button well", () => {
     const confirmButton = screen.getByText("confirm");
 
     expect(confirmButton).toBeInTheDocument();
   });
 
   it("renders carousels well", () => {
-    render(<Edit />);
-
     const square = screen.getAllByRole("button");
     const noneSelectedHeart = screen.getByAltText("none selected heart");
     const checkedMark = screen.getAllByAltText("checked");
@@ -37,15 +42,37 @@ describe("Edit component", () => {
     expect(checkedMark).toHaveLength(2);
   });
 
+  it("selected font appears when a square is clicked", () => {
+    const bebasNeue = screen.getByRole("button", { name: /none selected bebasNeue/i });
+    fireEvent.mouseDown(bebasNeue);
+    const selectedFontInput = screen.getByDisplayValue("TEXT");
+
+    expect(selectedFontInput).toBeInTheDocument();
+  });
+
+  it("input value can be changed", () => {
+    const hanuman = screen.getByRole("button", { name: /none selected hanuman/i });
+    fireEvent.mouseDown(hanuman);
+    const selectedFontInput = screen.getByDisplayValue("TEXT");
+    fireEvent.change(selectedFontInput, { target: { value: "CHANGED" } });
+    const changedInput = screen.getByDisplayValue("CHANGED");
+
+    expect(changedInput).toBeInTheDocument();
+  });
+
   it("selected sticker appears when a square is clicked", () => {
-    render(<Edit />);
-
-    const button = screen.getByRole('button', { name: /none selected heart/i });
-
-    fireEvent.click(button);
-
+    const heart = screen.getByRole("button", { name: /none selected heart/i });
+    fireEvent.mouseDown(heart);
     const selectedSticker = screen.getByAltText("selected sticker");
 
     expect(selectedSticker).toBeInTheDocument();
+  });
+
+  it("postEditRequest is called when confirm is pressed", () => {
+    const confirmButton = screen.getByText("confirm");
+
+    fireEvent.mouseDown(confirmButton);
+
+    expect(mocks.postEditReuqest).toHaveBeenCalled();
   });
 });
