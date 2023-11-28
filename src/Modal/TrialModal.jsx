6c82b3a79d0c48c2cabefd5569250e9e6be1d4e3
Youjin-cmd <y.kimiau@gmail.com>
2023-11-28 09:07:34 +0900

    Feat. update TrialModal with static images as buttons

--- src/Modal/TrialModal.jsx
+++ src/Modal/TrialModal.jsx
@@ -1,14 +1,66 @@
-import Modal from "./Modal";
-
 import useModalStore from "../store/modal";
+import useProgressStore from "../store/progress";
+import useGetTrialRequest from "../apis/useGetTrialRequest";
+
+import Modal from "./Modal";
+import Button from "../shared/Button";
+import LoadingArea from "../Loading/LoadingArea";
+import useTrialStore from "../store/trial";
 
 function TrialModal() {
   const { setShowTrialModal } = useModalStore();
+  const { selectedTrialVideo, setSelectedTrialVideo } = useTrialStore();
+  const { showLoading } = useProgressStore();
+  const getTrialRequest = useGetTrialRequest();
+
+  async function handleClickSubmit() {
+    await getTrialRequest(selectedTrialVideo);
+  }
 
   return (
     <Modal onClick={() => setShowTrialModal(false)}>
       <div className="flex flex-col items-center">
-        <span>texttexttexttexttexttexttexttexttexttexttexttext</span>
+        <h1 className="text-2xl mb-10">Choose a trial video</h1>
+        <div className="flex flex-col items-center">
+          <div className="flex mb-10">
+            <Button
+              className={`p-1 mr-5
+                ${selectedTrialVideo === "dog" && "ring-4 ring-red"}`}
+              onClick={() => setSelectedTrialVideo("dog")}
+            >
+              <img width="400px" src="/assets/trial_dog.png" />
+            </Button>
+            <Button
+              className={`p-1 mr-5
+                ${selectedTrialVideo === "dance" && "ring-4 ring-red"}`}
+              onClick={() => setSelectedTrialVideo("dance")}
+            >
+              <img width="400px" src="/assets/trial_dance.png" />
+            </Button>
+            <Button
+              className={`p-1 mr-5
+                ${selectedTrialVideo === "wedding" && "ring-4 ring-red"}`}
+              onClick={() => setSelectedTrialVideo("wedding")}
+            >
+              <img width="400px" src="/assets/trial_wedding.png" />
+            </Button>
+          </div>
+          <div className="relative h-16 w-80">
+            {showLoading && (
+              <LoadingArea
+                className={
+                  "absolute flex flex-col justify-center items-center h-16 w-80 rounded-md z-1 bg-white opacity-90"
+                }
+              />
+            )}
+            <Button
+              className="h-16 w-80 rounded-xl bg-blue text-xl text-white hover:bg-blueHover"
+              onClick={handleClickSubmit}
+            >
+              confirm
+            </Button>
+          </div>
+        </div>
       </div>
     </Modal>
   );
