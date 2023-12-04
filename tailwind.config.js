641ca8647c418d7b2c73c648d4a011ba75344347
Youjin-cmd <y.kimiau@gmail.com>
2023-12-05 01:01:31 +0900

    Setting. add TS to TailwindCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}", "./src/**/*.{ts,tsx}"],
   theme: {
     colors: {
       red: "#C32F2F",
