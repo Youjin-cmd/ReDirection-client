e3dd97bcfb526b15aba9182f81062670a7f1a92b
Youjin-cmd <y.kimiau@gmail.com>
2023-12-10 02:01:15 +0900

    Refactor. typescript migration (etc.)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss"
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}", "./src/**/*.{ts,tsx}"],
   theme: {
@@ -157,4 +158,4 @@ export default {
     },
   },
   plugins: [],
-};
+} satisfies Config
